5216bf164c95536ebd7c4f0ec2505348803f6dd3
srikarkowsika <68210945+srikarkowsika@users.noreply.github.com>
2020-07-12 23:17:32 -0400

    Add files via upload
    These files are under the tracker library folder in models folder.

---
+++ MatchupEntryModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class MatchupEntryModel
+    {
+
+        /// <summary>
+        /// The unique id that represents each Matchup Entry. 
+        /// </summary>
+        public int id { get; set; }
+
+       
+        /// <summary>
+        /// The unique identifier for the team
+        /// </summary>
+        public int TeamCompetingId { get; set; }
+
+        /// <summary>
+        /// Represents one team in the matchup. 
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents the score for this particular team. 
+        /// </summary>
+       
+        public double Score { get; set; }
+
+        /// <summary>
+        /// The unique identifier for the parent matchup (team).
+        /// </summary>
+        public int ParentMatchupId { get; set; }
+
+
+        /// <summary>
+        /// Represents the matchup that this team came
+        /// from as the winner. 
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialScore">
+       
+        
+        
+      
+    }
+}
---
+++ PrizeModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class PrizeModel
+    {
+        public int Id { get; set; }
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+
+        public PrizeModel(string id, string placeNumber, string placeName, string prizeAmount)
+        {
+
+            
+
+            int placeNumberValue = 0;
+            int.TryParse(placeNumber, out placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            
+            PlaceName = placeName;
+
+            
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+
+        }
+
+
+            public PrizeModel(string id, string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+            {
+
+                int idValue = 0;
+                int.TryParse(id, out idValue);
+                idValue = Id;
+
+
+
+                int placeNumberValue = 0;
+                int.TryParse(placeNumber, out placeNumberValue);
+                PlaceNumber = placeNumberValue;
+                PlaceName = placeName;
+
+                decimal prizeAmountValue = 0;
+                decimal.TryParse(prizeAmount, out prizeAmountValue);
+                PrizeAmount = prizeAmountValue;
+
+                double prizePercentageValue = 0;
+                double.TryParse(prizePercentage, out prizePercentageValue);
+                PrizePercentage = prizePercentageValue;
+            }
+
+        public PrizeModel()
+        {
+
+        }
+        
+    }
+}
